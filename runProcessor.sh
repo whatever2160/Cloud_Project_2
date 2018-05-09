fa4ac85d47d0ab8a82a8e246d9ee5facb1b2ec71
yaozican <zican.yao@gmail.com>
2018-05-09 23:50:01 +1000

    change structure ...

--- runProcessor.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export MPJ_HOME=/home/ubuntu/nick/mpj-v0_44/
-export PATH=$MPJ_HOME/bin:$PATH
-
-mpjrun.sh -np 4 /home/ubuntu/Cloud_Project_2-master/out/artifacts/cloudProject2_jar/cloudProject2.jar
