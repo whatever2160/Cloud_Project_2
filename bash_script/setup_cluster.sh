e3bea7233c12bd4985964428d11fae53e2d0df96
Mingchi Zhang <hlsnw2006@163.com>
2018-05-10 13:26:23 +1000

    Finally mofified all files.

--- bash_script/setup_cluster.sh
+++ bash_script/setup_cluster.sh
@@ -1,5 +1,5 @@
-curl -X POST -H 'Content-Type: application/json' http://admin:admin@115.146.95.108:5984/_cluster_setup -d "{\"action\": \"enable_cluster\", \"bind_address\":\"0.0.0.0\", \"username\": \"admin\", \"password\":\"admin\", \"port\": 5984, \"node_count\": \"2\", \"remote_node\": \"115.146.95.79\", \"remote_current_user\": \"admin\", \"remote_current_password\": \"admin\"}"
-curl -X POST -H 'Content-Type: application/json' http://admin:admin@115.146.95.108:5984/_cluster_setup -d "{\"action\": \"add_node\", \"host\":\"115.146.95.79\", \"port\": 5984, \"username\": \"admin\", \"password\":\"admin\"}"
+curl -X POST -H 'Content-Type: application/json' http://admin:admin@115.146.95.87:5984/_cluster_setup -d "{\"action\": \"enable_cluster\", \"bind_address\":\"0.0.0.0\", \"username\": \"admin\", \"password\":\"admin\", \"port\": 5984, \"node_count\": \"2\", \"remote_node\": \"115.146.95.212\", \"remote_current_user\": \"admin\", \"remote_current_password\": \"admin\"}"
+curl -X POST -H 'Content-Type: application/json' http://admin:admin@115.146.95.87:5984/_cluster_setup -d "{\"action\": \"add_node\", \"host\":\"115.146.95.212\", \"port\": 5984, \"username\": \"admin\", \"password\":\"admin\"}"
 curl -X POST "http://admin:admin@localhost:5984/_cluster_setup" -H 'Content-Type: application/json' -d '{"action": "finish_cluster"}'
 curl http://admin:admin@localhost:5984/_cluster_setup
 curl -X GET http://admin:admin@localhost:5984/_membership
