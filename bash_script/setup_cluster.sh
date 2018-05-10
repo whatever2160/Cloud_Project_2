19b67435a082d7c9abe24c04360b17eb03a99a98
Mingchi Zhang <hlsnw2006@163.com>
2018-05-10 10:27:57 +1000

    modified ansible, setup_cluster.sh

--- bash_script/setup_cluster.sh
+++ bash_script/setup_cluster.sh
@@ -1,3 +1,5 @@
-cd /home/ubuntu/
-echo "export MPJ_HOME=/home/ubuntu/mpj-v0_44" >> ~/.bashrc
-echo "export PATH=$MPJ_HOME/bin:$PATH" >> ~/.bashrc
+curl -X POST -H 'Content-Type: application/json' http://admin:admin@115.146.95.108:5984/_cluster_setup -d "{\"action\": \"enable_cluster\", \"bind_address\":\"0.0.0.0\", \"username\": \"admin\", \"password\":\"admin\", \"port\": 5984, \"node_count\": \"2\", \"remote_node\": \"115.146.95.32\", \"remote_current_user\": \"admin\", \"remote_current_password\": \"admin\"}"
+curl -X POST -H 'Content-Type: application/json' http://admin:admin@115.146.95.108:5984/_cluster_setup -d "{\"action\": \"add_node\", \"host\":\"115.146.95.32\", \"port\": 5984, \"username\": \"admin\", \"password\":\"admin\"}"
+curl -X POST "http://admin:admin@localhost:5984/_cluster_setup" -H 'Content-Type: application/json' -d '{"action": "finish_cluster"}'
+curl http://admin:admin@localhost:5984/_cluster_setup
+curl -X GET http://admin:admin@localhost:5984/_membership
